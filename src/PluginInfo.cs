26e1040e5acfca256069c21e3129ea9f915a98e1
DanielSchaack <d.schaack@web.de>
2026-08-30 22:16:31 +0200

    Version Bump

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,7 +1,7 @@
 namespace Randomizer {
     public class PluginInfo {
         public const string NAME = "Metal Hellsinger Randomizer";
-        public const string VERSION = "1.1.0";
+        public const string VERSION = "1.1.1";
         public const string GUID = "MetalHellsinger.Randomizer";
     }
 }
